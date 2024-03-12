76177cdfe0d8281322dd119398b0e03720f851a9
CmrCrabs <zayaanazam1234@hotmail.com>
2024-03-12 21:43:54 +0000

    duck.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -5,8 +5,6 @@ return {
     local lspconfig = require("lspconfig")
     --lspconfig.lua_ls.setup({})
     lspconfig.tsserver.setup({})
-    lspconfig.rust_analyzer.setup({})
-
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
