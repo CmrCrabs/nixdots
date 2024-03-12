ed91b5d36e3f56434cb026e5f1a7e10441e11341
CmrCrabs <zayaanazam1234@hotmail.com>
2024-03-12 21:43:54 +0000

    duck.

---
+++ nvim/lua/plugins/duck.lua
@@ -0,0 +1,9 @@
+return {
+    'tamton-aquib/duck.nvim',
+    config = function()
+        vim.keymap.set('n', '<leader>dd', function() require("duck").hatch() end, {})
+        vim.keymap.set('n', '<leader>dk', function() require("duck").cook() end, {})
+        vim.keymap.set('n', '<leader>da', function() require("duck").cook_all() end, {})
+        -- nnoremap <leader>dd :lua require("duck").hatch("ඞ")<CR>
+    end
+}
