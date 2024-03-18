344b9d61d6b9fc59b3c616c0489e6774bbd5443b
CmrCrabs <zayaanazam1234@hotmail.com>
2024-03-18 15:59:11 +0000

    misc nvim

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,17 +1,25 @@
 return {
+  {
     "nyoom-engineering/oxocarbon.nvim",
     lazy = false,
     name = "oxocarbon",
-    priority = 1000, 
+    priority = 1000,
     config = function()
       vim.opt.background = "dark"
-      vim.cmd.colorschem "oxocarbon"	  
+      vim.cmd.colorschem "oxocarbon"
       vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
       vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
       vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
       vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
       vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
     end,
+  },
+  {
+    "NvChad/nvim-colorizer.lua",
+    config = function ()
+      require 'colorizer'.setup()
+    end,
+  },
 }
 
 
